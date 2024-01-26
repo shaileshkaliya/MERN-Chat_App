cd8aba6df7c0c1368df7f0e40adadbcff6ec1c92
shaileshkaliya <shaileshkaliya124@gmail.com>
2024-01-26 18:17:55 +0530

    Corrected some minor mistakes in register functionality

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import logo from "../assets/loginLogo.png";
-import {Link, useNavigate} from "react-router-dom"
-import {ToastContainer, toast} from "react-toastify"
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from 'axios'
-import {registerRoute} from '../utils/APIRoutes'
-
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
   const signupData = {
@@ -25,65 +24,52 @@ function Register() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState(signupData);
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(handleValidation()){
-      const {username, email, password} = formData;
-      // const {data} = await axios.post(registerRoute, {
-      //   username, email, password
-      // });
+    if (handleValidation()) {
+      const { username, email, password } = formData;
       try {
-        const response = await axios.post(registerRoute, { username, email, password });
-        console.log('Registration successful:', response.data);
-
-        if (response.data.status === false) {
-            toast.error(response.data.message, toastOptions);
-        }
-
+        const response = await axios.post(registerRoute, {
+          username,
+          email,
+          password,
+        });
+        
         console.log(response.data);
-    } catch (error) {
-        console.error('Registration failed:', error.response.data);
+        if (response.data.status === false) {
+          toast.error(response.data.msg, toastOptions);
+        }
+        if (response.data.status === true) {
+          toast.success(response.data.msg, toastOptions);
+          navigate('/')
+        }
+        
+      } catch (error) {
+        console.error("Registration failed:", error.response.data);
+      }
     }
-
-      // if(response.data.status===false){
-      //   toast.error(data.msg, toastOptions);
-      // }
-      // console.log(response.data);
-    }
-  }
+  };
 
   const handleValidation = () => {
-    const {username, email, password, confirmPassword} = formData;
+    const { username, email, password, confirmPassword } = formData;
 
-    if(password != confirmPassword){
-      toast.error(
-        "Password & confirm password must be same",
-        toastOptions
-      )
+    if (password != confirmPassword) {
+      toast.error("Password & confirm password must be same", toastOptions);
       return false;
-    }else if(username.length < 3){
-      toast.error(
-        "Username should be greater than 3 characters",
-        toastOptions
-      )
+    } else if (username.length < 3) {
+      toast.error("Username should be greater than 3 characters", toastOptions);
       return false;
-    } else if(password.length < 8){
-      toast.error(
-        "Password length should be greater than 8",
-        toastOptions
-      )
+    } else if (password.length < 8) {
+      toast.error("Password length should be greater than 8", toastOptions);
       return false;
-    }else if(email===""){
-      toast.error(
-        "Email is required",
-        toastOptions
-      )
+    } else if (email === "") {
+      toast.error("Email is required", toastOptions);
       return false;
     }
 
     return true;
-  }
+  };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[#131324]">
@@ -96,16 +82,19 @@ function Register() {
         </div>
 
         <div className="w-full flex flex-col items-center h-full">
-          <form className="flex flex-col w-full items-center h-3/4 justify-evenly"
-            onSubmit={(e)=>handleSubmit()}
-            action=""
+          <form
+            className="flex flex-col w-full items-center h-3/4 justify-evenly"
+            onSubmit={(e) => handleSubmit(e)}
+            
           >
             <input
               type="text"
               name="username"
               placeholder="Username"
               value={formData.username}
-              onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
               required
               className="bg-transparent border-[#480eea] border-[1px] rounded-lg text-[#5b5f67] p-2 text-sm w-3/4"
             />
@@ -114,7 +103,9 @@ function Register() {
               name="email"
               placeholder="Email"
               value={formData.email}
-              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
               required
               className="bg-transparent border-[#480eea] border-[1px] rounded-lg text-[#5b5f67] p-2 text-sm w-3/4"
             />
@@ -123,7 +114,9 @@ function Register() {
               name="password"
               placeholder="Password"
               value={formData.password}
-              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
               required
               className="bg-transparent border-[#480eea] border-[1px] rounded-lg text-[#5b5f67] p-2 text-sm w-3/4"
             />
@@ -132,20 +125,23 @@ function Register() {
               name="confirmPassword"
               placeholder="Confirm Password"
               value={formData.confirmPassword}
-              onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, confirmPassword: e.target.value })
+              }
               required
               className="bg-transparent border-[#480eea] border-[1px] rounded-lg text-[#5b5f67] p-2 text-sm w-3/4"
             />
-            <button type="submit" className="w-3/4 bg-[#9979f1] text-white p-[4px] rounded-lg">
+            <button
+              type="submit"
+              className="w-3/4 bg-[#9979f1] text-white p-[4px] rounded-lg"
+            >
               CREATE USER
             </button>
           </form>
           <p className="text-white font-light sm:text-sm text-[12px]">
             ALREADY HAVE AN ACCOUNT ?{" "}
             <Link to="/login" className="text-[#100bd8]">
-              <span className="text-[#100bd8] cursor-pointer">
-                Login .
-              </span>{" "}
+              <span className="text-[#100bd8] cursor-pointer">Login .</span>{" "}
             </Link>
           </p>
         </div>
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -4,33 +4,23 @@ const bcrypt = require('bcryptjs');
 const register = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
-
-        const userCheck = await User.findOne({ username: username });
-        if (userCheck) {
-            return res.status(409).json({ message: "Username already exists", status: false });
-        }
-
-        const emailCheck = await User.findOne({ email: email });
-        if (emailCheck) {
-            return res.status(409).json({ message: "Email already exists", status: false });
-        }
-
+        const usernameCheck = await User.findOne({ username });
+        if (usernameCheck)
+          return res.json({ msg: "Username already used", status: false });
+        const emailCheck = await User.findOne({ email });
+        if (emailCheck)
+          return res.json({ msg: "Email already used", status: false });
         const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({
-            username: username,
-            email: email,
-            password: hashedPassword
+        const user = await User.create({
+          email,
+          username,
+          password: hashedPassword,
         });
-
-        const savedUser = await user.save();
-        const userWithoutPassword = savedUser.toJSON();
-        delete userWithoutPassword.password;
-
-        return res.status(201).json({ status: true, user: userWithoutPassword });
-    } catch (error) {
-        next(error);
-    }
+        delete user.password;
+        return res.json({ status: true, user });
+      } catch (ex) {
+        next(ex);
+      }
 };
 
 module.exports = { register };
